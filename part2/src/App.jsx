1d91df60f1d49530101ee26c881a61ff7da0560b
Milosz Kuzniewski <zydor2137@gmail.com>
2026-03-05 14:04:16 +0100

    Renamed folders to match Helsinki Fullstack course structure

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -22,19 +22,24 @@ const Total = ({ parts }) => {
 const Course = ({ course }) => (
   <div>
     <Header name={course.name} />
+   
     <Content parts={course.parts} />
+    
     <Total parts={course.parts} />
     
   </div>
 )
 
 const App = () => {
+  
   const courses = [
+    
     {
       id: 1,
       name: 'Half Stack application development',
       parts: [
-        { id: 1, name: 'Fundamentals of React', exercises: 10},
+        
+        { id: 1, name: 'Fundamentals of React', exercises: 10,},
         { id: 2, name: 'Using props to pass data', exercises: 7 },
         { id: 3, name: 'State of a component', exercises: 14 }
       ]
@@ -55,6 +60,8 @@ const App = () => {
       {courses.map(course =>
         <Course key={course.id} course={course} />
       )}
+
+      
     </div>
   )
 }
