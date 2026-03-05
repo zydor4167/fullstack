08fc9c519a1b7021ecb2be0a1384c5516cae5fb1
Milosz Kuzniewski <zydor2137@gmail.com>
2026-03-05 14:54:16 +0100

    part2c itp itd hehehe

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,113 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Filter = ({ filter, handleFilterChange }) => (
+  <div>
+    filter shown with <input value={filter} onChange={handleFilterChange} />
+  </div>
+)
+
+const PersonForm = ({
+  addPerson,
+  newName,
+  handleNameChange,
+  newNumber,
+  handleNumberChange
+}) => (
+  <form onSubmit={addPerson}>
+    <div>
+      name: <input value={newName} onChange={handleNameChange} />
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={handleNumberChange} />
+    </div>
+    <button type="submit">add</button>
+  </form>
+)
+
+const Person = ({ person }) => (
+  <p>
+    {person.name} {person.number}
+  </p>
+)
+
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map(person =>
+      <Person key={person.id} person={person} />
+    )}
+  </div>
+)
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+ 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const nameExists = persons.some(
+      person => person.name.toLowerCase() === newName.toLowerCase()
+    )
+
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter
+        filter={filter}
+        handleFilterChange={(e) => setFilter(e.target.value)}
+      />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={(e) => setNewName(e.target.value)}
+        newNumber={newNumber}
+        handleNumberChange={(e) => setNewNumber(e.target.value)}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={personsToShow} />
+    </div>
+  )
+}
+
+export default App
